f34a0c00e39b1f2cedb1e26da481d76954aa69fd
andrii <quadr988@gmail.com>
2022-05-21 20:38:42 +0300

    init

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
@@ -31,6 +32,19 @@ func main() {
 
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	tmpl := template.Must(template.ParseFiles("layout.html"))
+	http.HandleFunc("/template", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData{
+			PageTitle: "My TODO list",
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: true},
+				{Title: "Task 3", Done: true},
+			},
+		}
+		tmpl.Execute(w, data)
+	})
 
 	http.ListenAndServe(":8080", nil)
 }
